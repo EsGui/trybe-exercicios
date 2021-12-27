662bc4987996ab4eaa42a224c36897fed5e87d7c
Guilherme Azevedo <guilhermedagama2021100@gmail.com>
2021-12-27 18:19:07 -0300

    Exercicio 008.js feito

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios/ex008.js
@@ -0,0 +1,11 @@
+/* 
+8 - Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+*/
+
+let array = [];
+
+for (index = 1; index < 26; index += 1) {
+    array.push(index);
+}
+
+console.log(array)
