1179375303633ceaa51134f0d27a1339b2e5d0f4
Guilherme Azevedo <guilhermedagama2021100@gmail.com>
2021-12-27 21:05:02 -0300

    Exercicio 001.js do conteúdo de Lógica de Progração e Algoritmos feito

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios/ex001.js
@@ -0,0 +1,21 @@
+/*
+
+1 - O fatorial é a multiplicação de um número natural pelos seus antecessores, exceto o zero. Por exemplo:
+
+    O fatorial é representado pelo sinal !
+    ! = 4 x 3 x 2 x 1 = 24
+
+Com base nessas informações, crie um algoritmo que retorne o fatorial de 10.
+
+*/
+
+let num = 4;
+let fatorial = 1;
+
+for (let index = 1; index < num + 1; index += 1) {
+    fatorial *= index
+}
+
+console.log(fatorial);
+
+
