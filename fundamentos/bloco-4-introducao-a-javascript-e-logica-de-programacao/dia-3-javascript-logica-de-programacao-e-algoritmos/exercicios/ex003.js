b98f2a3166f8fcc5501b0f866f79a6c0dd9b80fa
Guilherme Azevedo <guilhermedagama2021100@gmail.com>
2021-12-28 15:06:45 -0300

    Exercicio 004.js do conteúdo de Lógica de Programação e Algoritmos feito

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios/ex003.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios/ex003.js
@@ -6,6 +6,8 @@
 
 Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
 
+Referência: Trybe
+
 */
 
 let array = ['java', 'javascript', 'python', 'html', 'css'];
