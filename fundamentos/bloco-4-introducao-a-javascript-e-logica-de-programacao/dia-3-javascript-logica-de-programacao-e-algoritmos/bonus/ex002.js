9217b39d70bef64bc1e4b246bccf2f4077252308
Guilherme Azevedo <guilhermedagama2021100@gmail.com>
2021-12-28 17:42:43 -0300

    Exercicio 002.js bonus do conteúdo de Lógica de Programação e Algoritmos

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/bonus/ex002.js
@@ -0,0 +1,20 @@
+/* 
+2 - Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+
+n = 5
+
+*
+**
+***
+****
+*****
+
+*/
+
+let asteriscos = "*"
+let reserve = "";
+
+for (let index = 0; index < 5; index += 1) {
+    reserve += asteriscos
+    console.log(reserve);
+}
