dc585554f0b8326262a11bccc4f01534bde45a8f
Guilherme Azevedo <guilhermedagama2021100@gmail.com>
2021-12-30 15:29:14 -0300

    Exercicio 003.js do conteúdo de Objetos e Funções feito

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios/funcoes/ex003.js
@@ -0,0 +1,33 @@
+/* 
+
+3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+
+    Array de teste: [2, 4, 6, 7, 10, 0, -3]; .
+    Valor esperado no retorno da função: 6 .
+
+*/
+
+function maiorValorDoArray(array) {
+    /* let array = [2, 3, 6, 7, 10, 1]; */
+    let array2 = [];
+    let prosseguir = 0
+    
+    for (let num in array) {
+        array2.push(array[num])
+        
+    }
+    
+    for (let index = 0; index < array2.length; index += 1) {
+        if (array2[prosseguir] > array2[index]) {
+            array2[prosseguir] = array2[index]
+        }
+    }
+    
+    for (let index = 0; index < array2.length; index += 1) {
+        if (array2[0] == array[index]) {
+            console.log(array.indexOf(array[index]));
+        }
+    }
+}
+
+maiorValorDoArray([2, 3, 6, 7, 10, 1]);
