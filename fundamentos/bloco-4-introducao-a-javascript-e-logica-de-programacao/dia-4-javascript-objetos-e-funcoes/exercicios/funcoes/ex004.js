13c1f359e2f8464772a873463b34b980c18f0723
Guilherme Azevedo <guilhermedagama2021100@gmail.com>
2021-12-30 15:53:52 -0300

    Exercicio 004.js do conteúdo de Objetos e Funções feito

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios/funcoes/ex004.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios/funcoes/ex004.js
@@ -9,23 +9,27 @@
 
 */
 
-let array = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
-let array2 = [];
-let prosseguir = 0
 
-for (let palavra in array) {
-    array2.push(array[palavra].length)   
-}
-
-for (let index = 0; index < array2.length; index += 1) {
-    if (array2[prosseguir] < array2[index]) {
-        array2[prosseguir] = array2[index];
+function maiorPalavra(array) { 
+    let array2 = [];
+    let prosseguir = 0
+    
+    for (let palavra in array) {
+        array2.push(array[palavra].length)   
+    }
+    
+    for (let index = 0; index < array2.length; index += 1) {
+        if (array2[prosseguir] < array2[index]) {
+            array2[prosseguir] = array2[index];
+        }
+    }
+    
+    for (let index = 0; index < array.length; index += 1) {
+        if (array[index].length === array2[0]) {
+            console.log(array[index]);
+        }
     }
 }
 
-for (let index = 0; index < array.length; index += 1) {
-    if (array[index].length === array2[0]) {
-        console.log(array[index]);
-    }
-}
 
+maiorPalavra(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']);
