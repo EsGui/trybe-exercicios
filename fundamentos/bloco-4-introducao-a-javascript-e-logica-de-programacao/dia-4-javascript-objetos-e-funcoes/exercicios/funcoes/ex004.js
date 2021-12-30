1162334730dbb2eff0addd65361c4957dbe92143
Guilherme Azevedo <guilhermedagama2021100@gmail.com>
2021-12-30 15:49:23 -0300

    Exercicio 004.js do conteúdo de Objetos e Funções feito

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios/funcoes/ex004.js
@@ -0,0 +1,31 @@
+/* 
+
+4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+
+    Array de teste: ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']; 
+
+    Valor esperado no retorno da função: Fernanda.
+
+
+*/
+
+let array = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+let array2 = [];
+let prosseguir = 0
+
+for (let palavra in array) {
+    array2.push(array[palavra].length)   
+}
+
+for (let index = 0; index < array2.length; index += 1) {
+    if (array2[prosseguir] < array2[index]) {
+        array2[prosseguir] = array2[index];
+    }
+}
+
+for (let index = 0; index < array.length; index += 1) {
+    if (array[index].length === array2[0]) {
+        console.log(array[index]);
+    }
+}
+
