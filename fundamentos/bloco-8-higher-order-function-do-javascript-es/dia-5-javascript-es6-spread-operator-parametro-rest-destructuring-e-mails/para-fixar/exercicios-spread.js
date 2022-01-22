fdbfe8bbab68aeabd8717c776780d05305ea5be1
Guilherme Azevedo <guilhermedagama2021100@gmail.com>
2022-01-21 23:40:56 -0300

    Exercício de fixação do conteúdo de spread feito

---
+++ fundamentos/bloco-8-higher-order-function-do-javascript-es/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mails/para-fixar/exercicios-spread.js
@@ -0,0 +1,11 @@
+// Faça uma lista com as suas frutas favoritas
+const specialFruit = ['Maçã', 'Uva', 'Pera'];
+
+// Faça uma lista de complementos que você gostaria de adicionar
+const additionalItens = ['Goiaba', 'Laranja', 'Banana'];
+
+const fruitSalad = (fruit, additional) => {
+    return juntaTudo = [...fruit, ...additional]
+};
+
+console.log(fruitSalad(specialFruit, additionalItens));
