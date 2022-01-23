7c30864832ddac4c57c033f35c265fdd4a94f0c5
Guilherme Azevedo <guilhermedagama2021100@gmail.com>
2022-01-23 15:47:25 -0300

    Questão 3 do dia 8.5 feito

--- fundamentos/bloco-8-higher-order-function-do-javascript-es/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mails/exercicios/ex002.js
+++ fundamentos/bloco-8-higher-order-function-do-javascript-es/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mails/exercicios/ex002.js
@@ -6,7 +6,6 @@ Dica: use parâmetro rest .
 */
 
 
-
 const sum = (...args) => args.reduce((accumulator, current) => accumulator + current, 0);
 
 console.log(sum(4, 7, 8, 9, 60))
---
+++ fundamentos/bloco-8-higher-order-function-do-javascript-es/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mails/exercicios/ex003.js
@@ -0,0 +1,31 @@
+/* 
+
+3 - Escreva a função personLikes , que dado um objeto de parâmetro que representa uma pessoa, retorna todos os gostos daquela pessoa, conforme mostrado abaixo:
+
+    Dica: use object destructuring.
+
+    Créditos: Trybe 
+
+*/
+
+const alex = {
+    name: 'Alex',
+    age: 26,
+    likes: ['fly fishing'],
+    nationality: 'Australian',
+};
+
+const gunnar = {
+    name: 'Gunnar',
+    age: 30,
+    likes: ['hiking', 'scuba diving', 'taking pictures'],
+    nationality: 'Icelandic',
+};
+
+// complete a assinatura da função abaixo
+const personLikes = ({name, age, likes}) => `${name} is ${age} year old and likes ${likes.join(', ')}.`;
+
+console.log(personLikes(alex));
+console.log(personLikes(gunnar));
+
+
